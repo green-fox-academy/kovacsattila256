8c9481ac0227874ce4136fa6bf19b9fa6d253ae1
kovacsattila256 <kovacs.attila.ab@gmail.com>
2018-05-17 12:23:46 +0200

    pushy!

---
+++ week-02/day-4/exercise03/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // Draw the canvas' diagonals.
+            // If it starts from the upper-left corner it should be green, otherwise it should be red.
+
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(0, 0, 1600, 850);
+
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine(0, 850, 1600, 0);
+
+        }
+    }
+}
