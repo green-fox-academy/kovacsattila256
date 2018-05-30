1e7a78d8906c8c4971f1151efdaa97c1d9d9eeb9
kovacsattila256 <kovacs.attila.ab@gmail.com>
2018-05-30 13:59:32 +0200

    updating old workshop tasks

--- week-01/day-4/exercise_01_HelloMe.cs
+++ week-01/day-4/exercise_01_HelloMe.cs
@@ -7,6 +7,7 @@ namespace GreenFox
         static void Main(string[] args)
         {
             // Modify this program to greet you instead of the World!
+            //  Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, Attila!");
         }
     }
