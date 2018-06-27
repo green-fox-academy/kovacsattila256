5516c9004eaec1cc4b522ff9f793a5351b7db4cc
kovacsattila256 <kovacs.attila.ab@gmail.com>
2018-06-27 14:17:51 +0200

    new tasks

---
+++ week-07/day-3/task1-counter/Services/CounterService.cs
@@ -0,0 +1,28 @@
+﻿using Counter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Counter.Services
+{
+    public class CounterService : ICounter
+    {
+        private Number storedValue;
+
+        public CounterService()
+        {
+            storedValue = new Number() { MyNumber = 0 };
+        }
+
+        public Number GetNumber()
+        {
+            return storedValue;
+        }
+
+        public void Increase()
+        {
+            storedValue.MyNumber++;
+        }
+    }
+}
